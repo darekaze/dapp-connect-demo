ef7287da0848acbd7533fcaf0d2949e81fcb134a
DaRwin <darekaze@gmail.com>
2022-10-28 16:43:33 +0800

    fix: add connected chain name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,24 @@
-import { useAccount, useConnect, useDisconnect, useEnsName } from "wagmi"
+import { useAccount, useConnect, useDisconnect, useEnsName, useNetwork } from 'wagmi'
 
 export const HomePage = () => {
   const { address, connector, isConnected } = useAccount()
   const { data: ensName } = useEnsName({ address })
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect()
+  const { chain } = useNetwork()
+
+  const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
   const { disconnect } = useDisconnect()
 
   if (isConnected) {
     return (
       <div>
         <div>{ensName ? `${ensName} (${address})` : address}</div>
-        <div>Connected to {connector?.name}</div>
+        <div>
+          Connected to {chain?.name} using {connector?.name}
+        </div>
 
-        <button className="btn btn-error" onClick={() => disconnect()}>Disconnect</button>
+        <button className="btn btn-error" onClick={() => disconnect()}>
+          Disconnect
+        </button>
       </div>
     )
   }
@@ -28,10 +33,8 @@ export const HomePage = () => {
           onClick={() => connect({ connector })}
         >
           {connector.name}
-          {!connector.ready && " (unsupported)"}
-          {isLoading &&
-            connector.id === pendingConnector?.id &&
-            " (connecting)"}
+          {!connector.ready && ' (unsupported)'}
+          {isLoading && connector.id === pendingConnector?.id && ' (connecting)'}
         </button>
       ))}
 
